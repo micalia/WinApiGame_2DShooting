09c7d14dec46ea8c6e192dd755c1af273d1b933a
micalia <60741767+micalia@users.noreply.github.com>
2024-06-19 17:35:23 +0900

    Init

---
+++ GameCoding/GameCoding/Game.h
@@ -0,0 +1,22 @@
+#pragma once
+class Game
+{
+public:
+	Game();
+	~Game();
+
+public:
+	void Init(HWND hwnd);
+	void Update();
+	void Render();
+
+private:
+	HWND _hwnd = {};
+	HDC _hdc = {};
+
+private:
+	RECT _rect;
+	HDC _hdcBack = {};
+	HBITMAP _bmpBack = {};
+};
+
